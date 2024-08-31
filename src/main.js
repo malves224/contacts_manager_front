5407c35de550421ca155148a8e435a3c0fffc095
Matheus Alves <malves224@gmail.com>
2024-08-31 14:32:40 -0300

    config vue router, bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).mount('#app')
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+
+// Import BootstrapVue 3
+import BootstrapVue3 from 'bootstrap-vue-3';
+
+const app = createApp(App);
+
+// Make BootstrapVue available throughout your project
+app.use(router);
+app.use(BootstrapVue3);
+
+app.mount('#app');
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    name: 'LoginView',
+    component: import('../views/LoginView.vue'),
+  },
+  {
+    path: '/contacts',
+    name: 'ContactsView',
+    component: import('../views/ContactsView.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
