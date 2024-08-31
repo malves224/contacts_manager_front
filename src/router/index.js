95311e2602243a653bc7647430c008a7ed77943f
Matheus Alves <malves224@gmail.com>
2024-08-31 20:06:18 -0300

    fix(router) path login

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
-    path: '/',
+    path: '/login',
     name: 'LoginView',
     component: import('../views/LoginView.vue'),
   },
