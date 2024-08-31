536b4b26435a31a171a1e53f5cc3d50662f11e7e
Matheus Alves <malves224@gmail.com>
2024-08-31 20:07:10 -0300

    feat(UserService)#login and #register

---
+++ src/services/UserService.js
@@ -0,0 +1,20 @@
+import HttpService from './http';
+
+class UserService extends HttpService {
+  constructor() {
+    super();
+  }
+
+  async login(email, password) {
+    const response = await this.client.post('/login', { email, password })
+    this.storage.set({ token: response.data.token, id: response.data.id })
+    return response.data
+  }
+
+  async register(name, email, password) {
+    const response = await this.client.post('users', { name, email, password })
+    return response.data
+  }
+  
+}
+export default UserService;
