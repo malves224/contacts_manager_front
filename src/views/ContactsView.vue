61c41586197a738387baf1a615bfb7a2c1b69770
Matheus Alves <malves224@gmail.com>
2024-08-31 23:54:37 -0300

    feat(contactsView) initial structure

---
+++ src/views/ContactsView.vue
@@ -0,0 +1,65 @@
+<template lang="">
+  <div>
+    <div class="navbar-container">
+      <b-button variant="outline-danger">Sair</b-button>
+    </div>
+    <div class="contacts-container">
+        <div class="list-contacts-container">
+          <div class="search-contacts-container">
+            <b-form-input placeholder="Pesquisar"></b-form-input>
+            <b-button variant="outline-primary">
+              Criar contato
+            </b-button>
+          </div>
+          <div class="items-contacts-container">
+            <b-list-group>
+              <b-list-group-item>Item 1</b-list-group-item>
+              <b-list-group-item>Item 2</b-list-group-item>
+              <b-list-group-item>Item 3</b-list-group-item>
+            </b-list-group>
+          </div>
+        </div>
+        <div class="google-maps-container">
+          maps      
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+import UserService from '../services/UserService';
+export default {
+  data() {
+    return {
+      userService: new UserService()
+    }
+  },
+  beforeMount() {
+    const token = this.userService.storage.get().token
+    if (!token) {
+      this.$router.push('/login')
+    }
+  },
+}
+</script>
+<style >
+  .navbar-container {
+    display: flex;
+    justify-content: end;
+    padding: 0 20px;
+    width: 100%;
+    background-color: aquamarine;
+    height: 40px;
+  }
+  .contacts-container {
+    display: flex;
+  }
+  .list-contacts-container {
+    display: flex;
+    width: 40%;
+    flex-direction: column;
+  }
+  .search-contacts-container {
+    display: flex;
+    flex-direction: row;
+  }
+</style>
