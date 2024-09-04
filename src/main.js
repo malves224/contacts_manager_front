52cf4aa650c7245747bf2aab11b96e80a392b25b
Matheus Alves <malves224@gmail.com>
2024-09-04 10:27:59 -0300

    chore: install bootstrap icons

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router from './router';
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 // Import BootstrapVue 3
 import BootstrapVue3 from 'bootstrap-vue-3';
